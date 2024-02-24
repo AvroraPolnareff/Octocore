2eaf00ef13f24f1a29c46e65657474399c448573
AuroraPolnareff <araintheskywith@gmail.com>
2024-02-24 21:04:47 +0300

    haha funny

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,6 @@ fn main() -> anyhow::Result<()> {
         control.clone()
     );
     run_input(midi_in, in_port, pitch, volume, pitch_bend, control)
-
-
 }
 
 fn create_sound(
@@ -68,7 +66,7 @@ fn run_input(
     pitch: Shared<f64>,
     volume: Shared<f64>,
     pitch_bend: Shared<f64>,
-    control: Shared<f64>
+    control: Shared<f64>,
 ) -> anyhow::Result<()> {
     println!("\nOpening connection");
     let in_port_name = midi_in.port_name(&in_port)?;
@@ -81,7 +79,7 @@ fn run_input(
                 if let MidiMsg::ChannelVoice { channel: _, msg } = msg {
                     println!("Received {msg:?}");
                     match msg {
-                        ChannelVoiceMsg::NoteOn { note, velocity} => {
+                        ChannelVoiceMsg::NoteOn { note, velocity } => {
                             pitch.set_value(midi_hz(note as f64));
                             volume.set_value(velocity as f64 / 127.0);
                             pitch_bend.set_value(1.0);
@@ -89,41 +87,41 @@ fn run_input(
                         }
                         ChannelVoiceMsg::NoteOff { note, velocity: _ } => {
                             if pitch.value() == midi_hz(note as f64) {
-                                control.set_value(-1.0)
+                                control.set_value(-1.0);
                             }
                         }
                         ChannelVoiceMsg::PitchBend { bend } => {
-                            pitch_bend.set_value(pitch_bend_factor(bend))
+                            pitch_bend.set_value(pitch_bend_factor(bend));
                         }
                         _ => {}
                     }
                 }
             },
-            ()
+            (),
         )
         .unwrap();
     println!("Connection open, reading input from '{in_port_name}'");
-    let _ = input::<String>().msg("press enter to exit...\n").get();
+
+    let _ = input::<String>().msg("(press enter to exit)...\n").get();
     println!("Closing connection");
     Ok(())
 }
 
 fn pitch_bend_factor(bend: u16) -> f64 {
-    2.0_f64.powf(((bend as f64 - 8192.0) / 8182.0) / 12.0)
+    2.0_f64.powf(((bend as f64 - 8192.0) / 8192.0) / 12.0)
 }
 
 fn run_output(
     pitch: Shared<f64>,
     volume: Shared<f64>,
     pitch_bend: Shared<f64>,
-    control: Shared<f64>
+    control: Shared<f64>,
 ) {
     let host = cpal::default_host();
     let device = host
         .default_output_device()
-        .expect("failed to find default output device");
+        .expect("failed to find a default output device");
     let config = device.default_output_config().unwrap();
-
     match config.sample_format() {
         SampleFormat::F32 => {
             run_synth::<f32>(pitch, volume, pitch_bend, control, device, config.into())
@@ -134,7 +132,7 @@ fn run_output(
         SampleFormat::U16 => {
             run_synth::<u16>(pitch, volume, pitch_bend, control, device, config.into())
         }
-        _ => panic!("unssupported format")
+        _ => panic!("Unsupported format"),
     }
 }
 
@@ -144,25 +142,27 @@ fn run_synth<T: SizedSample + FromSample<f64>>(
     pitch_bend: Shared<f64>,
     control: Shared<f64>,
     device: Device,
-    config: StreamConfig
+    config: StreamConfig,
 ) {
     std::thread::spawn(move || {
         let sample_rate = config.sample_rate.0 as f64;
         let mut sound = create_sound(pitch, volume, pitch_bend, control);
         sound.set_sample_rate(sample_rate);
-        
+
         let mut next_value = move || sound.get_stereo();
         let channels = config.channels as usize;
         let err_fn = |err| eprintln!("an error occurred on stream: {err}");
         let stream = device
             .build_output_stream(
                 &config,
-                move |data: &mut [T], _| {
+                move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
                     write_data(data, channels, &mut next_value)
                 },
                 err_fn,
-                None
-            ).unwrap();
+                None,
+            )
+            .unwrap();
+
         stream.play().unwrap();
         loop {
             std::thread::sleep(std::time::Duration::from_millis(1));
@@ -173,7 +173,7 @@ fn run_synth<T: SizedSample + FromSample<f64>>(
 fn write_data<T: SizedSample + FromSample<f64>>(
     output: &mut [T],
     channels: usize,
-    next_sample: &mut dyn FnMut() -> (f64, f64)
+    next_sample: &mut dyn FnMut() -> (f64, f64),
 ) {
     for frame in output.chunks_mut(channels) {
         let sample = next_sample();
@@ -181,7 +181,7 @@ fn write_data<T: SizedSample + FromSample<f64>>(
         let right: T = T::from_sample(sample.1);
 
         for (channel, sample) in frame.iter_mut().enumerate() {
-            *sample = if channel & 1 == 0 { left } else { right }
+            *sample = if channel & 1 == 0 { left } else { right };
         }
     }
 }
