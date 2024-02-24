c2956cf382f9829f81b7bd4e73d72a4bedc0582c
AuroraPolnareff <araintheskywith@gmail.com>
2024-02-24 20:22:38 +0300

    simple midi synth

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,154 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::{Device, FromSample, SampleFormat, SizedSample, StreamConfig};
-use fundsp::hacker::{hammond_hz, multipass, reverb_stereo, sine, sine_hz, soft_saw_hz, square_hz, wave64, Wave64, square, saw_hz};
-use fundsp::prelude::AudioUnit64;
+use fundsp::hacker::{hammond_hz, multipass, reverb_stereo, sine, sine_hz, soft_saw_hz, square_hz, wave64, Wave64, square, saw_hz, lfo, Shared, var, triangle, shared};
+use fundsp::prelude::{adsr_live, AudioUnit64};
 use std::sync::Arc;
+use anyhow::bail;
+use fundsp::math::midi_hz;
+use midi_msg::{ChannelVoiceMsg, MidiMsg};
+use midir::{Ignore, MidiInput, MidiInputPort};
+use read_input::prelude::*;
+fn main() -> anyhow::Result<()> {
 
-/// This is the main function that is the entry point when we launch the
-/// binary, either directly or with `cargo run`.
-fn main() {
-    // Change the `create_sine_440` function to any of the functions
-    // that create a `Box<dyn AudioUnit64>` below, to change the
-    // sound that's generated.
-    let audio_graph = create_simple_fm();
+    let mut midi_in = MidiInput::new("midir reading input")?;
+    let in_port = get_midi_device(&mut midi_in)?;
 
-    // This function starts the thread that creates the audio and sends
-    // it to CPAL so that we can hear it.
-    run_output(audio_graph);
-
-    // The audio is being played on a thread, and will run infinitely.
-    // As soon as the main function exits, the sound will stop, so we
-    // can sleep the main thread for a while so we can hear it.
-    // Change the duration to play the sound for more or less time.
-    let duration = 2;
-    std::thread::sleep(std::time::Duration::from_secs(duration));
+    let pitch = shared(0.0);
+    let volume = shared(0.0);
+    let pitch_bend = shared(0.0);
+    let control = shared(0.0);
+    
+    run_output(
+        pitch.clone(),
+        volume.clone(),
+        pitch_bend.clone(),
+        control.clone()
+    );
+    run_input(midi_in, in_port, pitch, volume, pitch_bend, control)
+    
+    
 }
 
-/// This function determines the sample format, which depends on your system,
-/// then starts the synth, passing along the audio graph that will generate
-/// the sound to be played.
-fn run_output(audio_graph: Box<dyn AudioUnit64>) {
-    let host = cpal::default_host();
-    let device = host
-        .default_output_device()
-        .expect("failed to find a default output device");
-    let config = device.default_output_config().unwrap();
-    match config.sample_format() {
-        SampleFormat::F32 => run_synth::<f32>(audio_graph, device, config.into()),
-        SampleFormat::I16 => run_synth::<i16>(audio_graph, device, config.into()),
-        SampleFormat::U16 => run_synth::<u16>(audio_graph, device, config.into()),
+fn create_sound(
+    pitch: Shared<f64>,
+    volume: Shared<f64>,
+    pitch_bend: Shared<f64>,
+    control: Shared<f64>
+) -> Box<dyn AudioUnit64> {
+    Box::new(
+        var(&pitch_bend) * var(&pitch)
+            >> triangle() * (var(&control) >> adsr_live(0.1, 0.2, 0.4, 0.2) * var(&volume))
+    )
+}
 
-        _ => panic!("Unsupported format"),
+fn get_midi_device(midi_in: &mut MidiInput) -> anyhow::Result<MidiInputPort> {
+    midi_in.ignore(Ignore::None);
+    let in_ports = midi_in.ports();
+    if in_ports.is_empty() {
+        bail!("No MIDI devices attached")
+    } else {
+        println!(
+            "Chose MIDI device {}",
+            midi_in.port_name(&in_ports[1]).unwrap()
+        );
+        Ok(in_ports[1].clone())
     }
 }
 
-/// This function takes an audio graph as an input, along with some the audio
-/// device and config, and starts a thread that will play the audio. The thread
-/// will loop infinitely until the programme exits.
+fn run_input(
+    midi_in: MidiInput,
+    in_port: MidiInputPort,
+    pitch: Shared<f64>,
+    volume: Shared<f64>,
+    pitch_bend: Shared<f64>,
+    control: Shared<f64>
+) -> anyhow::Result<()> {
+    println!("\nOpening connection");
+    let in_port_name = midi_in.port_name(&in_port)?;
+    let _conn_in = midi_in
+        .connect(
+            &in_port,
+            "midir-read-input",
+            move |_stamp, message, _| {
+                let (msg, _len) = MidiMsg::from_midi(message).unwrap();
+                if let MidiMsg::ChannelVoice { channel: _, msg } = msg {
+                    println!("Received {msg:?}");
+                    match msg {
+                        ChannelVoiceMsg::NoteOn { note, velocity} => {
+                            pitch.set_value(midi_hz(note as f64));
+                            volume.set_value(velocity as f64 / 127.0);
+                            pitch_bend.set_value(1.0);
+                            control.set_value(1.0);
+                        }
+                        ChannelVoiceMsg::NoteOff { note, velocity: _ } => {
+                            if pitch.value() == midi_hz(note as f64) {
+                                control.set_value(-1.0)
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            },
+            ()
+        )
+        .unwrap();
+    println!("Connection open, reading input from '{in_port_name}'");
+    let _ = input::<String>().msg("press enter to exit...\n").get();
+    println!("Closing connection");
+    Ok(())
+}
+
+fn run_output(
+    pitch: Shared<f64>,
+    volume: Shared<f64>,
+    pitch_bend: Shared<f64>,
+    control: Shared<f64>
+) {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("failed to find default output device");
+    let config = device.default_output_config().unwrap();
+
+    match config.sample_format() {
+        SampleFormat::F32 => {
+            run_synth::<f32>(pitch, volume, pitch_bend, control, device, config.into())
+        }
+        SampleFormat::I16 => {
+            run_synth::<i16>(pitch, volume, pitch_bend, control, device, config.into())
+        }
+        SampleFormat::U16 => {
+            run_synth::<u16>(pitch, volume, pitch_bend, control, device, config.into())
+        }
+        _ => panic!("unssupported format")
+    }
+}
+
 fn run_synth<T: SizedSample + FromSample<f64>>(
-    mut audio_graph: Box<dyn AudioUnit64>,
+    pitch: Shared<f64>,
+    volume: Shared<f64>,
+    pitch_bend: Shared<f64>,
+    control: Shared<f64>,
     device: Device,
-    config: StreamConfig,
+    config: StreamConfig
 ) {
     std::thread::spawn(move || {
         let sample_rate = config.sample_rate.0 as f64;
-        audio_graph.set_sample_rate(sample_rate);
-
-        // This is a function that is used to get the next audio sample. It is
-        // written using the closure syntax, so looks a bit different from
-        // normal function definition.
-        let mut next_value = move || audio_graph.get_stereo();
-
+        let mut sound = create_sound(pitch, volume, pitch_bend, control);
+        sound.set_sample_rate(sample_rate);
+        
+        let mut next_value = move || sound.get_stereo();
         let channels = config.channels as usize;
         let err_fn = |err| eprintln!("an error occurred on stream: {err}");
         let stream = device
             .build_output_stream(
                 &config,
-                move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+                move |data: &mut [T], _| {
                     write_data(data, channels, &mut next_value)
                 },
                 err_fn,
-                None,
-            )
-            .unwrap();
-
+                None
+            ).unwrap();
         stream.play().unwrap();
         loop {
             std::thread::sleep(std::time::Duration::from_millis(1));
@@ -79,12 +156,10 @@ fn run_synth<T: SizedSample + FromSample<f64>>(
     });
 }
 
-/// This function is passed to the output audio stream and is used to generate
-/// audio samples and send them to the audio device.
 fn write_data<T: SizedSample + FromSample<f64>>(
     output: &mut [T],
     channels: usize,
-    next_sample: &mut dyn FnMut() -> (f64, f64),
+    next_sample: &mut dyn FnMut() -> (f64, f64)
 ) {
     for frame in output.chunks_mut(channels) {
         let sample = next_sample();
@@ -92,62 +167,8 @@ fn write_data<T: SizedSample + FromSample<f64>>(
         let right: T = T::from_sample(sample.1);
 
         for (channel, sample) in frame.iter_mut().enumerate() {
-            *sample = if channel & 1 == 0 { left } else { right };
+            *sample = if channel & 1 == 0 { left } else { right }
         }
     }
 }
 
-// ------------------------------------------------------------------
-// You can use any of the functions in this section to make the audio
-// graph. Just replace the function call in `main` at the top.
-
-/// Simple sine wave at 440 Hz which is standard tuning for A4
-fn create_sine_440() -> Box<dyn AudioUnit64> {
-    let synth = sine_hz(440.0);
-
-    Box::new(synth)
-}
-
-/// C major chord created by summing waves! Sine by default, but try uncommenting
-/// the other wave types.
-fn create_c_major() -> Box<dyn AudioUnit64> {
-    let synth = sine_hz(261.6) + sine_hz(329.628) + sine_hz(391.995);
-    // let synth = square_hz(261.6) + square_hz(329.628) + square_hz(391.995);
-    // let synth = soft_saw_hz(261.6) + soft_saw_hz(329.628) + soft_saw_hz(391.995);
-    // let synth = hammond_hz(261.6) + hammond_hz(329.628) + hammond_hz(391.995);
-
-    Box::new(synth)
-}
-
-/// Load and play a sample
-fn create_sample() -> Box<dyn AudioUnit64> {
-    let wave =
-        Arc::new(Wave64::load("samples/closed_high_hat.wav").expect("Could not find sample file."));
-    let left = wave64(&wave, 0, None);
-    let right = wave64(&wave, 1, None);
-    let synth = left | right;
-
-    Box::new(synth)
-}
-
-// Load and play a sample, but this time we add reverb
-fn create_sample_with_reverb() -> Box<dyn AudioUnit64> {
-    let wave =
-        Arc::new(Wave64::load("samples/closed_high_hat.wav").expect("Could not find sample file."));
-    let left = wave64(&wave, 0, None);
-    let right = wave64(&wave, 1, None);
-    let synth = (left | right) >> (multipass() & (0.2 * reverb_stereo(10.0, 3.0)));
-
-    Box::new(synth)
-}
-
-// Simple FM synthesiser taken from the FunDSP docs
-fn create_simple_fm() -> Box<dyn AudioUnit64> {
-    // Frequency
-    let f = 220.0;
-    // Modulation index
-    let m = 0.0;
-    let synth = (saw_hz(f * 0.25) * f * m + f) >> sine();
-
-    Box::new(synth)
-}
