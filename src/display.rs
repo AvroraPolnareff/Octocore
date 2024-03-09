631228a3895dbcc8f6ab839933a0964c2c5f076a
AuroraPolnareff <araintheskywith@gmail.com>
2024-03-09 19:09:17 +0300

    add dest to the display

--- src/display.rs
+++ src/display.rs
@@ -20,7 +20,7 @@ static DEFAULT_TYPEFACE: OnceCell<Typeface> = OnceCell::new();
 
 pub fn render_param(
   name: &str,
-  value: f64,
+  value: String,
   point: impl Into<Point>,
   canvas: &Canvas
 ) {
@@ -37,13 +37,14 @@ pub fn render_param(
     &paint
   );
   canvas.draw_str(
-    format!("{:.2}", value),
+    value.as_str(),
     (x, y + 40.0),
     &Font::from_typeface(default_typeface(), 40.0),
     &paint
   );
-
 }
+
+fn fmt_float(f: f64) -> String { format!("{:.2}", f) }
 
 pub fn render_image(params: &SynthParams, state: UIState, pixels: &mut [u8; 2048 * 160]) -> [u8; 2048 * 160] {
   let image_info = ImageInfo::new((960, 160), ColorType::RGB565, AlphaType::Opaque, ColorSpace::new_srgb());
@@ -59,37 +60,42 @@ pub fn render_image(params: &SynthParams, state: UIState, pixels: &mut [u8; 2048
 
   let page = state.page.lock().unwrap();
   let op_subpage = state.op_subpage.lock().unwrap();
+  let dest = state.lfo_dest.lock().unwrap();
   let calc_param_pos = |ord: f32| (120. * ord - 120. / 2. - 40., 60.);
   match *page {
     Page::Op1 => {
       match *op_subpage {
         OpPage::Tone => {
-          render_param("Volume", params.op1.volume.value(), calc_param_pos(1.), canvas);
-          render_param("Ratio", params.op1.ratio.value(), calc_param_pos(2.), canvas);
+          render_param("Volume", fmt_float(params.op1.volume.value()), calc_param_pos(1.), canvas);
+          render_param("Ratio", fmt_float(params.op1.ratio.value()), calc_param_pos(2.), canvas);
 
         }
         OpPage::Amp => {
-          render_param("Attack", params.op1.adsr_params.a.value(), calc_param_pos(1.), canvas);
-          render_param("Decay", params.op1.adsr_params.d.value(), calc_param_pos(2.), canvas);
-          render_param("Sustain", params.op1.adsr_params.s.value(), calc_param_pos(3.), canvas);
-          render_param("Release", params.op1.adsr_params.r.value(), calc_param_pos(4.), canvas);
+          render_param("Attack", fmt_float(params.op1.adsr_params.a.value()), calc_param_pos(1.), canvas);
+          render_param("Decay", fmt_float(params.op1.adsr_params.d.value()), calc_param_pos(2.), canvas);
+          render_param("Sustain", fmt_float(params.op1.adsr_params.s.value()), calc_param_pos(3.), canvas);
+          render_param("Release", fmt_float(params.op1.adsr_params.r.value()), calc_param_pos(4.), canvas);
         }
       }
     }
     Page::Op2 => {
       match *op_subpage {
         OpPage::Tone => {
-          render_param("Volume", params.op2.volume.value(), calc_param_pos(1.), canvas);
-          render_param("Ratio", params.op2.ratio.value(), calc_param_pos(2.), canvas);
+          render_param("Volume", fmt_float(params.op2.volume.value()), calc_param_pos(1.), canvas);
+          render_param("Ratio", fmt_float(params.op2.ratio.value()), calc_param_pos(2.), canvas);
 
         }
         OpPage::Amp => {
-          render_param("Attack", params.op2.adsr_params.a.value(), calc_param_pos(1.), canvas);
-          render_param("Decay", params.op2.adsr_params.d.value(), calc_param_pos(2.), canvas);
-          render_param("Sustain", params.op2.adsr_params.s.value(), calc_param_pos(3.), canvas);
-          render_param("Release", params.op2.adsr_params.r.value(), calc_param_pos(4.), canvas);
+          render_param("Attack", fmt_float(params.op2.adsr_params.a.value()), calc_param_pos(1.), canvas);
+          render_param("Decay", fmt_float(params.op2.adsr_params.d.value()), calc_param_pos(2.), canvas);
+          render_param("Sustain", fmt_float(params.op2.adsr_params.s.value()), calc_param_pos(3.), canvas);
+          render_param("Release", fmt_float(params.op2.adsr_params.r.value()), calc_param_pos(4.), canvas);
         }
       }
+    }
+    Page::Modulation => {
+      let dest = dest.to_owned().1;
+      render_param("Op 2", dest.name, calc_param_pos(1.), canvas)
     }
     _ => {}
   }
